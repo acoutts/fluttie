f3545e6436a7f0990cd10c56d0f476dc14ae25b0
Andrew Coutts <andrewjohncoutts@gmail.com>
2020-01-25 11:45:55 -0500

    update

--- android/src/main/java/de/simolus3/fluttie/FluttiePlugin.java
+++ android/src/main/java/de/simolus3/fluttie/FluttiePlugin.java
@@ -25,256 +25,262 @@ import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
 import io.flutter.view.TextureRegistry;
 
-public class FluttiePlugin implements MethodCallHandler, Application.ActivityLifecycleCallbacks {
-	
-	public static void registerWith(Registrar registrar) {
-		FluttiePlugin plugin = new FluttiePlugin(registrar);
-		plugin.setUp();
-	}
+public class FluttiePlugin implements FlutterPlugin, ActivityAware {
 
-	private Registrar registrar;
+  public static void registerWith(Registrar registrar) {
+    FluttiePlugin plugin = new FluttiePlugin(registrar);
+    plugin.setUp();
+  }
 
-	private SparseArray<FluttieAnimation> managedAnimations = new SparseArray<>();
+  private Registrar registrar;
 
-	private SparseArray<LottieComposition> loadedCompositions = new SparseArray<>();
-	private AtomicInteger compositionRequestCounter = new AtomicInteger();
+  private SparseArray<FluttieAnimation> managedAnimations = new SparseArray<>();
 
-	private RenderingThreads renderingThreads;
+  private SparseArray<LottieComposition> loadedCompositions =
+      new SparseArray<>();
+  private AtomicInteger compositionRequestCounter = new AtomicInteger();
 
-	private FluttiePlugin(Registrar registrar) {
-		this.registrar = registrar;
-	}
+  private RenderingThreads renderingThreads;
 
-	private void setUp() {
-		final MethodChannel channel = new MethodChannel(registrar.messenger(), "fluttie/methods");
+  private FluttiePlugin(Registrar registrar) { this.registrar = registrar; }
 
-		channel.setMethodCallHandler(this);
+  private void setUp() {
+    final MethodChannel channel =
+        new MethodChannel(registrar.messenger(), "fluttie/methods");
 
-		registrar.activity().getApplication().registerActivityLifecycleCallbacks(this);
+    channel.setMethodCallHandler(this);
 
-		/*
-		 When multiple threads are working on rendering multiple animations simultaneously, at some
-		 we will inevitably send two texture updates to the flutter engine with a really short time
-		 delay in between. This, as it seems, will crash the engine :(
-		 There is an issue that could be related to this, https://github.com/flutter/flutter/issues/14169
-		 that we're waiting on to be fixed. After that is fixed, we can enable multithreaded rendering,
-		 this code should otherwise work. If it still crashes, we can still take a look into that
-		 later on and report a new issue to the Flutter team.
-		 */
-		renderingThreads = new RenderingThreads(1);
-		renderingThreads.start();
-	}
+    registrar.activity().getApplication().registerActivityLifecycleCallbacks(
+        this);
 
-	Drawable.Callback getAnimationCallback() {
-		return registrar.view();
-	}
+    /*
+     When multiple threads are working on rendering multiple animations
+     simultaneously, at some we will inevitably send two texture updates to the
+     flutter engine with a really short time delay in between. This, as it
+     seems, will crash the engine :( There is an issue that could be related to
+     this, https://github.com/flutter/flutter/issues/14169 that we're waiting on
+     to be fixed. After that is fixed, we can enable multithreaded rendering,
+     this code should otherwise work. If it still crashes, we can still take a
+     look into that later on and report a new issue to the Flutter team.
+     */
+    renderingThreads = new RenderingThreads(1);
+    renderingThreads.start();
+  }
 
-	private List<FluttieAnimation> getAllManagedAnimations() {
-		List<FluttieAnimation> animations = new ArrayList<>();
+  Drawable.Callback getAnimationCallback() { return registrar.view(); }
 
-		for (int i = 0; i < managedAnimations.size(); i++) {
-			animations.add(managedAnimations.get(managedAnimations.keyAt(i)));
-		}
+  private List<FluttieAnimation> getAllManagedAnimations() {
+    List<FluttieAnimation> animations = new ArrayList<>();
 
-		return animations;
-	}
+    for (int i = 0; i < managedAnimations.size(); i++) {
+      animations.add(managedAnimations.get(managedAnimations.keyAt(i)));
+    }
 
-	private FluttieAnimation getManagedAnimation(MethodCall call) {
-		int id = call.argument("id");
-		return managedAnimations.get(id);
-	}
+    return animations;
+  }
 
-	RenderingThreads getRenderingThreads() {
-		return renderingThreads;
-	}
+  private FluttieAnimation getManagedAnimation(MethodCall call) {
+    int id = call.argument("id");
+    return managedAnimations.get(id);
+  }
 
-	@Override
-	public void onMethodCall(MethodCall call, Result result) {
-		switch (call.method) {
-			case "isAvailable":
-				result.success(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP);
-				return;
-			case "loadAnimation":
-				String sourceType = call.argument("source_type");
-				String sourceData = call.argument("source");
+  RenderingThreads getRenderingThreads() { return renderingThreads; }
 
-				int requestId = compositionRequestCounter.getAndIncrement();
-				loadComposition(requestId, sourceType, sourceData, result);
+  @Override
+  public void onMethodCall(MethodCall call, Result result) {
+    switch (call.method) {
+    case "isAvailable":
+      result.success(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP);
+      return;
+    case "loadAnimation":
+      String sourceType = call.argument("source_type");
+      String sourceData = call.argument("source");
 
-				break;
-			case "prepareAnimation":
-				int compositionId = call.argument("composition");
-				LottieComposition composition = loadedCompositions.get(compositionId);
-				if (composition == null) {
-					result.error(
-							"invalid",
-							"There is no prepared animation for that request id: "
-									+ compositionId, null);
-					return;
-				}
+      int requestId = compositionRequestCounter.getAndIncrement();
+      loadComposition(requestId, sourceType, sourceData, result);
 
-				int repeatCount = call.argument("repeat_count");
-				int repeatMode = call.argument("repeat_reverse") ?
-						ValueAnimator.REVERSE : ValueAnimator.RESTART;
-				int durationMillis = call.argument("duration");
+      break;
+    case "prepareAnimation":
+      int compositionId = call.argument("composition");
+      LottieComposition composition = loadedCompositions.get(compositionId);
+      if (composition == null) {
+        result.error("invalid",
+                     "There is no prepared animation for that request id: " +
+                         compositionId,
+                     null);
+        return;
+      }
 
-				double preferredWidth = call.argument("pref_size_h");
-				double preferredHeight = call.argument("pref_size_h");
-				float scale = 1;
-				if (preferredWidth > 0 && preferredHeight > 0) {
-					double scaleX = preferredWidth / (float) composition.getBounds().height();
-					double scaleY = preferredHeight / (float) composition.getBounds().width();
+      int repeatCount = call.argument("repeat_count");
+      int repeatMode = call.argument("repeat_reverse") ? ValueAnimator.REVERSE
+                                                       : ValueAnimator.RESTART;
+      int durationMillis = call.argument("duration");
 
-					scale = (float) Math.min(scaleX, scaleY);
-				}
+      double preferredWidth = call.argument("pref_size_h");
+      double preferredHeight = call.argument("pref_size_h");
+      float scale = 1;
+      if (preferredWidth > 0 && preferredHeight > 0) {
+        double scaleX =
+            preferredWidth / (float)composition.getBounds().height();
+        double scaleY =
+            preferredHeight / (float)composition.getBounds().width();
 
-				TextureRegistry.SurfaceTextureEntry texture =
-						registrar.textures().createSurfaceTexture();
+        scale = (float)Math.min(scaleX, scaleY);
+      }
 
-				String imagesPath = call.argument("img_assets_path");
+      TextureRegistry.SurfaceTextureEntry texture =
+          registrar.textures().createSurfaceTexture();
 
-				FluttieAnimation animation = new FluttieAnimation(
-						this, texture, composition, scale, imagesPath);
-				animation.setRepeatOptions(repeatCount, repeatMode);
-				if (durationMillis > 0)
-					animation.setDuration(durationMillis);
+      String imagesPath = call.argument("img_assets_path");
 
-				managedAnimations.put((int) texture.id(), animation);
-				result.success(texture.id());
-				return;
-			case "startAnimation":
-				getManagedAnimation(call).startAnimation();
-				result.success(null);
-				return;
-			case "resumeAnimation":
-				getManagedAnimation(call).resumeAnimation();
-				result.success(null);
-				return;
-			case "pauseAnimation":
-				getManagedAnimation(call).pauseAnimation();
-				result.success(null);
-				return;
-			case "endAnimation":
-				boolean reset = call.argument("reset_start");
-				getManagedAnimation(call).stopAnimation(reset);
-				result.success(null);
-				return;
-			case "disposeAnimation":
-				FluttieAnimation anim = getManagedAnimation(call);
-				anim.stopAndRelease();
-				managedAnimations.remove(anim.getId());
-				//if scheduled to be drawn, cancel
-				renderingThreads.getQueue().removeAnimation(anim);
-				result.success(null);
-				return;
-			default:
-				result.notImplemented();
-		}
-	}
+      FluttieAnimation animation =
+          new FluttieAnimation(this, texture, composition, scale, imagesPath);
+      animation.setRepeatOptions(repeatCount, repeatMode);
+      if (durationMillis > 0)
+        animation.setDuration(durationMillis);
 
-	private void loadComposition(final int requestId, String sourceType, String source, final Result result) {
-		LottieListener<LottieComposition> listener = new LottieListener<LottieComposition>() {
-			@Override
-			public void onResult(@Nullable LottieComposition composition) {
-				try {
-					if (composition == null) {
-						Log.e("FluttiePlugin", "Could not load composition");
-						result.error("Could not load composition", "CompositionLoadError", null);
-						return;
-					}
+      managedAnimations.put((int)texture.id(), animation);
+      result.success(texture.id());
+      return;
+    case "startAnimation":
+      getManagedAnimation(call).startAnimation();
+      result.success(null);
+      return;
+    case "resumeAnimation":
+      getManagedAnimation(call).resumeAnimation();
+      result.success(null);
+      return;
+    case "pauseAnimation":
+      getManagedAnimation(call).pauseAnimation();
+      result.success(null);
+      return;
+    case "endAnimation":
+      boolean reset = call.argument("reset_start");
+      getManagedAnimation(call).stopAnimation(reset);
+      result.success(null);
+      return;
+    case "disposeAnimation":
+      FluttieAnimation anim = getManagedAnimation(call);
+      anim.stopAndRelease();
+      managedAnimations.remove(anim.getId());
+      // if scheduled to be drawn, cancel
+      renderingThreads.getQueue().removeAnimation(anim);
+      result.success(null);
+      return;
+    default:
+      result.notImplemented();
+    }
+  }
 
-					if (!composition.getWarnings().isEmpty()) {
-						Log.w("FluttiePlugin", "You tried to load a composition that has some warnings, it might not be displayed correctly");
-						for (String warning : composition.getWarnings()) {
-							Log.w("FluttiePlugin", warning);
-						}
-					}
+  private void loadComposition(final int requestId, String sourceType,
+                               String source, final Result result) {
+    LottieListener<LottieComposition> listener = new LottieListener<
+        LottieComposition>() {
+      @Override
+      public void onResult(@Nullable LottieComposition composition) {
+        try {
+          if (composition == null) {
+            Log.e("FluttiePlugin", "Could not load composition");
+            result.error("Could not load composition", "CompositionLoadError",
+                         null);
+            return;
+          }
 
-					loadedCompositions.append(requestId, composition);
+          if (!composition.getWarnings().isEmpty()) {
+            Log.w(
+                "FluttiePlugin",
+                "You tried to load a composition that has some warnings, it might not be displayed correctly");
+            for (String warning : composition.getWarnings()) {
+              Log.w("FluttiePlugin", warning);
+            }
+          }
 
-					result.success(requestId);
-				} catch (Throwable e) {
-					Log.e("FluttiePlugin", "Could not add JSON value to event stream");
-					result.error(e.getMessage(), "CompositionLoadError", null);
-				}
-			}
-		};
-		LottieListener<Throwable> failure = new LottieListener<Throwable>() {
-			@Override
-			public void onResult(Throwable result) {
-				Log.e("FluttiePlugin", "Failure loading resource: " + result);
-			}
-		};
+          loadedCompositions.append(requestId, composition);
 
-		switch (sourceType) {
-			case "inline":
-				LottieCompositionFactory.fromJsonString(source, null)
-						.addListener(listener)
-						.addFailureListener(failure);
-				break;
-			case "asset":
-				String key = registrar.lookupKeyForAsset(source);
-				LottieCompositionFactory.fromAsset(registrar.context(), key)
-						.addListener(listener)
-						.addFailureListener(failure);
-				break;
-			default:
-				result.error("Unknown source type: " + sourceType, "UnknownSourceType", null);
-		}
-	}
+          result.success(requestId);
+        } catch (Throwable e) {
+          Log.e("FluttiePlugin", "Could not add JSON value to event stream");
+          result.error(e.getMessage(), "CompositionLoadError", null);
+        }
+      }
+    };
+    LottieListener<Throwable> failure = new LottieListener<Throwable>() {
+      @Override
+      public void onResult(Throwable result) {
+        Log.e("FluttiePlugin", "Failure loading resource: " + result);
+      }
+    };
 
-	@Override
-	public void onActivityCreated(Activity activity, Bundle savedInstanceState) {}
+    switch (sourceType) {
+    case "inline":
+      LottieCompositionFactory.fromJsonString(source, null)
+          .addListener(listener)
+          .addFailureListener(failure);
+      break;
+    case "asset":
+      String key = registrar.lookupKeyForAsset(source);
+      LottieCompositionFactory.fromAsset(registrar.context(), key)
+          .addListener(listener)
+          .addFailureListener(failure);
+      break;
+    default:
+      result.error("Unknown source type: " + sourceType, "UnknownSourceType",
+                   null);
+    }
+  }
 
-	@Override
-	public void onActivityStarted(Activity activity) {
-		if (activity != registrar.activity())
-			return;
-		
-		Log.d("FluttiePlugin", "Activity restarted!");
-		if (renderingThreads != null) {
-			Log.d("FluttiePlugin", "Starting rendering threads");
-			renderingThreads.start();
-		}
-	}
+  @Override
+  public void onActivityCreated(Activity activity, Bundle savedInstanceState) {}
 
-	@Override
-	public void onActivityResumed(Activity activity) {
-		if (activity != registrar.activity())
-			return;
-		
-		for (FluttieAnimation anim : getAllManagedAnimations()) {
-			if (anim.isPausedButNotByUser()) {
-				anim.setPausedButNotByUser(false);
-				anim.resumeAnimation();
-			}
-		}
-	}
+  @Override
+  public void onActivityStarted(Activity activity) {
+    if (activity != registrar.activity())
+      return;
 
-	@Override
-	public void onActivityPaused(Activity activity) {
-		if (activity != registrar.activity())
-			return;
+    Log.d("FluttiePlugin", "Activity restarted!");
+    if (renderingThreads != null) {
+      Log.d("FluttiePlugin", "Starting rendering threads");
+      renderingThreads.start();
+    }
+  }
 
-		for (FluttieAnimation anim : getAllManagedAnimations()) {
-			if (anim.isPlaying()) {
-				anim.pauseAnimation();
-				anim.setPausedButNotByUser(true);
-			}
-		}
-	}
+  @Override
+  public void onActivityResumed(Activity activity) {
+    if (activity != registrar.activity())
+      return;
 
-	@Override
-	public void onActivityStopped(Activity activity) {
-		if (activity != registrar.activity())
-			return;
-		
-		renderingThreads.stop();
-	}
+    for (FluttieAnimation anim : getAllManagedAnimations()) {
+      if (anim.isPausedButNotByUser()) {
+        anim.setPausedButNotByUser(false);
+        anim.resumeAnimation();
+      }
+    }
+  }
 
-	@Override
-	public void onActivitySaveInstanceState(Activity activity, Bundle outState) {}
+  @Override
+  public void onActivityPaused(Activity activity) {
+    if (activity != registrar.activity())
+      return;
 
-	@Override
-	public void onActivityDestroyed(Activity activity) {}
+    for (FluttieAnimation anim : getAllManagedAnimations()) {
+      if (anim.isPlaying()) {
+        anim.pauseAnimation();
+        anim.setPausedButNotByUser(true);
+      }
+    }
+  }
+
+  @Override
+  public void onActivityStopped(Activity activity) {
+    if (activity != registrar.activity())
+      return;
+
+    renderingThreads.stop();
+  }
+
+  @Override
+  public void onActivitySaveInstanceState(Activity activity, Bundle outState) {}
+
+  @Override
+  public void onActivityDestroyed(Activity activity) {}
 }
